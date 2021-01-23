b325f6bc7353791dc9c0c3b357c03504efbd8633
Midgetman Low <kevinmlowg@yahoo.com>
2021-01-23 10:17:57 -0800

    added to_sql and updated app.py for flask app

--- app.py
+++ app.py
@@ -4,14 +4,25 @@ import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
+from sqlalchemy import MetaData
 
 from flask import Flask, jsonify
+
 
 
 #################################################
 # Database Setup
 #################################################
-engine = create_engine("sqlite:///titanic.sqlite")
+rds_connection_string = "postgres:postgres@localhost:5432/project2_db"
+engine = create_engine(f'postgresql://{rds_connection_string}')
+
+
+m = MetaData()
+m.reflect(engine)
+for table in m.tables.values():
+    print(table.name)
+    for column in table.c:
+        print(column.name)
 
 # reflect an existing database into a new model
 Base = automap_base()
@@ -19,7 +30,7 @@ Base = automap_base()
 Base.prepare(engine, reflect=True)
 
 # Save reference to the table
-Passenger = Base.classes.passenger
+covid_data = Base.classes.covid_data
 
 #################################################
 # Flask Setup
@@ -36,19 +47,19 @@ def welcome():
     """List all available api routes."""
     return (
         f"Available Routes:<br/>"
-        f"/api/v1.0/names<br/>"
-        f"/api/v1.0/passengers"
+        f"/api/v1.0/hospital_name<br/>"
+        f"/api/v1.0/covid_data"
     )
 
 
-@app.route("/api/v1.0/names")
+@app.route("/api/v1.0/hospital_name")
 def names():
     # Create our session (link) from Python to the DB
     session = Session(engine)
 
     """Return a list of all passenger names"""
     # Query all passengers
-    results = session.query(Passenger.name).all()
+    results = session.query(covid_data.hospital_name).all()
 
     session.close()
 
@@ -58,28 +69,28 @@ def names():
     return jsonify(all_names)
 
 
-@app.route("/api/v1.0/passengers")
-def passengers():
-    # Create our session (link) from Python to the DB
-    session = Session(engine)
+# @app.route("/api/v1.0/passengers")
+# def passengers():
+#     # Create our session (link) from Python to the DB
+#     session = Session(engine)
 
-    """Return a list of passenger data including the name, age, and sex of each passenger"""
-    # Query all passengers
-    results = session.query(Passenger.name, Passenger.age, Passenger.sex).all()
+#     """Return a list of passenger data including the name, age, and sex of each passenger"""
+#     # Query all passengers
+#     results = session.query(Passenger.name, Passenger.age, Passenger.sex).all()
 
-    session.close()
+#     session.close()
 
-    # Create a dictionary from the row data and append to a list of all_passengers
-    all_passengers = []
-    for name, age, sex in results:
-        passenger_dict = {}
-        passenger_dict["name"] = name
-        passenger_dict["age"] = age
-        passenger_dict["sex"] = sex
-        all_passengers.append(passenger_dict)
+#     # Create a dictionary from the row data and append to a list of all_passengers
+#     all_passengers = []
+#     for name, age, sex in results:
+#         passenger_dict = {}
+#         passenger_dict["name"] = name
+#         passenger_dict["age"] = age
+#         passenger_dict["sex"] = sex
+#         all_passengers.append(passenger_dict)
 
-    return jsonify(all_passengers)
+#     return jsonify(all_passengers)
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
